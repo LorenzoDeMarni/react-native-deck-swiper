8b93dadd17a0a2fa1d6a4fd53f0a494176306448
Lorenzo DeMarni <88600983+LorenzoDeMarni@users.noreply.github.com>
2025-04-05 17:51:31 -0400

    test 2

--- Swiper.js
+++ Swiper.js
@@ -762,7 +762,7 @@ class Swiper extends Component {
   }
 
   pushCardToStack = (renderedCards, index, position, key, firstCard) => {
-    console.log("Using Lorenzo's Repo")
+    console.log("Using Lorenzo's Repo test 2")
     const { cards } = this.props
     const stackCardZoomStyle = this.calculateStackCardZoomStyle(position)
     const stackCard = this.props.renderCard(cards[index], index)
